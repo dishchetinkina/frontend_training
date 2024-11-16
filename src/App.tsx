da3720bfbd9dac13b3e9fb6d91fac5c7df0e75f9
Di <mac@MacBook-Air-mac-2.local>
2024-11-16 14:52:53 +0300

    Добавила роутинг code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { Counter } from "./component/counter";
+import React, { Suspense } from "react";
 import './index.scss';
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            fyhjvjgjgj
-            <Counter/>
+            <Link to={'/'}>Главная страница</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
